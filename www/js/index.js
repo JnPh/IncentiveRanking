231e6436924b3b7e03e468a13e1d42a458f3b417
JnPh <JP@MacBook-Air-de-Jean-Philippe.local>
2016-03-22 22:58:51 -0400

    fix loading label

--- www/js/index.js
+++ www/js/index.js
@@ -41,7 +41,7 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
              
-        $(".event.listening").hide();
+        //$(".event.listening").hide();
       
         CheckSupportedVersion();
  
